2060329301ca8a7b05647575ade964d788e59013
tobb422 <tobb422@gmail.com>
2020-06-29 21:26:59 +0900

    create Tag Entity

---
+++ modules/domain/src/main/scala/domain/stock/entities/Tag.scala
@@ -0,0 +1,22 @@
+package domain.stock.entities
+
+import java.time.ZonedDateTime
+
+import shared.ddd.{Entity, Identifier}
+
+case class Tag(
+  id: Tag.Id,
+  name: String,
+  color: Tag.Color,
+  createdAt: ZonedDateTime,
+  updatedAt: ZonedDateTime
+) extends Entity {
+  override type Id = Tag.Id
+}
+
+object Tag {
+  case class Id(value: String) extends Identifier {
+    override type IdType = String
+  }
+  case class Color(value: String)
+}
