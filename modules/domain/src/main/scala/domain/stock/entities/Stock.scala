501bf913435700cd9f86bd0a1389914c329dea2a
tobb422 <tobb422@gmail.com>
2020-06-29 21:31:38 +0900

    create Stock Entity

--- modules/domain/src/main/scala/domain/stock/entities/Stock.scala
+++ modules/domain/src/main/scala/domain/stock/entities/Stock.scala
@@ -2,7 +2,20 @@ package domain.stock.entities
 
 import java.time.ZonedDateTime
 
-case class StockItem(id: String, url: String, createdAt: ZonedDateTime, updatedAt: ZonedDateTime)
-case class Tag(id: String, name: String, color: String)
+import shared.ddd.{Entity, Identifier}
 
-case class Stock(id: String, itemId: StockItem.Id, tags: Seq[Tag], createdAt: ZonedDateTime, updatedAt: ZonedDateTime)
+case class Stock(
+  id: Stock.Id,
+  itemId: StockItem.Id,
+  tags: List[Tag.Id],
+  createdAt: ZonedDateTime,
+  updatedAt: ZonedDateTime
+) extends Entity {
+  override type Id = Stock.Id
+}
+
+object Stock {
+  case class Id(value: String) extends Identifier {
+    override type IdType = String
+  }
+}
